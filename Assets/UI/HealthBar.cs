6bd98b9ba5b08b5e307f15aeda7424c09b5fc13e
Kofoedsan <kofoedsan@gmail.com>
2022-03-13 15:01:12 +0100

    HealthBarWorking

---
+++ Assets/UI/HealthBar.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    PlayerStats stats;
+    private float currentHealth;
+    Text healthText;
+
+    Image imgColor;
+
+    void Start() {
+        stats = GameObject.Find("PlayerKnight").GetComponent<PlayerStats>();
+        healthText = GameObject.Find("HealthBarText").GetComponent<Text>();
+        imgColor = GetComponent<Image>();
+    }
+
+    void Update() {
+        healthText.text = stats.currentHealth.ToString()+"%";
+
+        if (stats.currentHealth <= 80) {
+            imgColor.color= new Color32(0, 255, 80, 100);
+        } 
+        
+        if (stats.currentHealth <= 60) {
+            imgColor.color = new Color32(200, 200, 55, 100);
+        }
+
+        if (stats.currentHealth <= 40) {
+            imgColor.color = new Color32(180, 130, 40, 100);
+        }
+
+        if (stats.currentHealth <= 20) {
+            imgColor.color = new Color32(220, 40, 20, 100);
+        }
+
+    }
+}
